59ded6de9ee416a5a0a5489a7415b88b491a31e9
Tareesh21 <saitareeshreddy12@gmail.com>
2024-12-09 15:45:10 -0800

    code changes

---
+++ cmostwater.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public:
+        int maxArea(vector<int>& h) {
+            int i = 0, n = h.size(), j = n - 1;
+            int s = 0, ma = -1;
+            while(i < j){
+                s = (j - i)*min(h[i], h[j]);
+                ma = max(s, ma);
+                if (h[i] < h[j]) {
+                    i++;
+                } else 
+                {
+                    j--;
+                }
+            }
+            return ma;
+        }
+    }
+};
+    
